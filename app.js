c53cd59006e5bd5ef06f6cbf9fa4350a1be1ba3b
laurenmcelaney <45574516+laurenmcelaney@users.noreply.github.com>
2019-03-29 16:28:25 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function onReady() {
-const toDos = [];
+let toDos=[];
    let id = 0;
-   function renderTheUI(){
+
 const addToDoForm = document.getElementById('addToDoForm')};
 function createNewToDo() {
   const newToDoText = document.getElementById('newToDoText');
@@ -9,18 +9,22 @@ function createNewToDo() {
   toDos.push({
       title: newToDoText.value,
       complete: false,
-      id: id.value
+      id: ++id
     });
 
     newToDoText.value = '';
 
 }
+function deleteToDo(id) {
+  toDos = toDos.filter(item =>
+    { return toDos.filter(toDo => toDo.id !== id);}) }
+
 function renderTheUI() {
   const toDoList = document.getElementById('toDoList');
 
   toDoList.textContent = '';
 
-
+}
   toDos.forEach(function(toDo) {
     const newLi = document.createElement('li');
           const checkbox = document.createElement('input');
@@ -34,21 +38,18 @@ function renderTheUI() {
           newLi.appendChild(checkbox);
           newLi.appendChild(deleteButton);
 
-          function deleteToDo() {
-            toDos = toDos.filter(item => { return item != toDo; }); }
-
             deleteButton.addEventListener('click', event => {
               event.preventDefault();
               renderTheUI();
             });
     });
-  }
+  
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
   });
   renderTheUI();
-}
+
 
 window.onload = function() {
   onReady();
